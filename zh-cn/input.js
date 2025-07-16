a8ab96a525a16ce5eafb9a3477496d3629e4f111
Zaak Tung Wu <laowuservice@163.com>
2025-07-16 09:37:35 +0800

    Add files via upload

--- zh-cn/input.js
+++ zh-cn/input.js
@@ -7,20 +7,26 @@ var goal_minute;
 var goal_second;
 var repeat_time;
 var repeat_unit;
+var date_type;
+var if_add;
+var if_end;
+var end_year;
+var end_month;
+var end_date;
 
 function skip(){
   if (!judge()) return false;
   if (goal_hour == '') goal_hour = 0;
   if (goal_minute == '') goal_minute = 0;
   if (goal_second == '') goal_second = 0;
-  location.assign(encodeURI('./main.html?title=' + title + '&year=' + goal_year + '&month=' + goal_month + '&date=' + goal_date + '&hour=' + goal_hour + '&minute=' + goal_minute + '&second=' + goal_second + '&repeat_time=' + repeat_time + '&repeat_unit=' + repeat_unit));
+  location.assign(encodeURI('./main.html?title=' + title + '&year=' + goal_year + '&month=' + goal_month + '&date=' + goal_date + '&hour=' + goal_hour + '&minute=' + goal_minute + '&second=' + goal_second + '&repeat_time=' + repeat_time + '&repeat_unit=' + repeat_unit + '&date_type=' + date_type + '&if_add=' + if_add + '&if_end=' + if_end + '&end_year=' + end_year + '&end_month=' + end_month + '&end_date=' + end_date));
 }
 
 function judge(){
   var goal = new Date();
   goal.setFullYear(goal_year, goal_month - 1, goal_date);
   goal.setHours(goal_hour, goal_minute, goal_second);
-  if (!(goal instanceof Date && !isNaN(goal.getTime())) || goal_year < 1 || goal_month < 1 || goal_date < 1 || goal_hour < 0 || goal_minute < 0 || goal_second < 0 || goal_month > 12 || goal_date > 31 || goal_hour > 23 || goal_minute > 59 || goal_second > 59) {
+  if (!(goal instanceof Date && !isNaN(goal.getTime())) || goal_year < 1 || goal_month < 1 || goal_date < 1 || goal_hour < 0 || goal_minute < 0 || goal_second < 0 || (date_type == 'solar' && goal_month > 12) || (date_type == 'lunar' && goal_month > 13) || goal_date > 31 || goal_hour > 23 || goal_minute > 59 || goal_second > 59) {
     alert('时间日期格式不合法！');
     return false;
   } else if (title.length > 10) {
@@ -28,6 +34,12 @@ function judge(){
     return false;
   } else if (isNaN(repeat_time)) {
     alert('重复格式错误！');
+    return false;
+  }
+  if (if_end === 0) return true;
+  goal.setFullYear(end_year, end_month - 1, end_date);
+  if (!(goal instanceof Date && !isNaN(goal.getTime())) || end_year < 1 || end_month < 1 || end_date < 1 || (date_type == 'solar' && end_month > 12) || (date_type == 'lunar' && end_month > 13) || end_date > 31) {
+    alert('结束日期格式不合法！');
     return false;
   }
   return true;
@@ -44,9 +56,17 @@ function submit(){
   if (document.getElementById("if_repeat").checked) {
     repeat_time = document.getElementById("repeat_time").value;
     repeat_unit = document.getElementById("repeat_unit").value;
-  } else {
-    repeat_time = repeat_unit = 0;
-  }
+  } else repeat_time = repeat_unit = 0;
+  var radios = document.getElementsByName("type");
+  if (radios[0].checked) date_type = 'solar';
+  else date_type = 'lunar';
+  if_add = Number(document.getElementById("if_add").checked);
+  if_end = Number(document.getElementById("if_end").checked);
+  if (document.getElementById("if_end").checked) {
+    end_year = document.getElementById("end_year").value;
+    end_month = document.getElementById("end_month").value;
+    end_date = document.getElementById("end_date").value;
+  } else end_year = end_month = end_date = 0;
   skip();
 }
 
@@ -60,7 +80,7 @@ document.getElementById('open_file').addEventListener('change', function(event) 
         reader.onload = function(e) {
             var text = e.target.result;
             var word = text.split('\n');
-            if (word.length <= 9) {
+            if (word.length <= 15) {
               title = word[0];
               goal_year = parseInt(word[1]);
               goal_month = parseInt(word[2]);
@@ -69,7 +89,13 @@ document.getElementById('open_file').addEventListener('change', function(event) 
               goal_minute = (word.length > 4) ? parseInt(word[5]) : 0;
               goal_second = (word.length > 4) ? parseInt(word[6]) : 0;
               repeat_time = (word.length > 7) ? parseInt(word[7]) : 0;
-              repeat_unit = (word.length > 7) ? parseInt(word[8]) : 0;
+              repeat_unit = (word.length > 7) ? word[8] : 'day';
+              date_type = (word.length > 9) ? word[9] : 'solar';
+              if_add = (word.length > 9) ? parseInt(word[10]) : 0;
+              if_end = (word.length > 9) ? parseInt(word[11]) : 0;
+              end_year = (word.length > 9) ? parseInt(word[12]) : 0;
+              end_month = (word.length > 9) ? parseInt(word[13]) : 0;
+              end_date = (word.length > 9) ? parseInt(word[14]) : 0;
               skip();
             } else {
               alert('请选择一个.cdd格式的文件。');
@@ -84,6 +110,10 @@ document.getElementById('open_file').addEventListener('change', function(event) 
 document.getElementById('if_repeat').checked = false;
 document.getElementById('repeat_time').disabled = true;
 document.getElementById('repeat_unit').disabled = true;
+document.getElementById('if_end').checked = false;
+document.getElementById('end_year').disabled = true;
+document.getElementById('end_month').disabled = true;
+document.getElementById('end_date').disabled = true;
 document.getElementById('if_repeat').addEventListener('click', function() {
   if (this.checked == false) {
     document.getElementById('repeat_time').disabled = true;
@@ -91,5 +121,16 @@ document.getElementById('if_repeat').addEventListener('click', function() {
   } else {
     document.getElementById('repeat_time').disabled = false;
     document.getElementById('repeat_unit').disabled = false;
+  }
+});
+document.getElementById('if_end').addEventListener('click', function() {
+  if (this.checked == false) {
+    document.getElementById('end_year').disabled = true;
+    document.getElementById('end_month').disabled = true;
+    document.getElementById('end_date').disabled = true;
+  } else {
+    document.getElementById('end_year').disabled = false;
+    document.getElementById('end_month').disabled = false;
+    document.getElementById('end_date').disabled = false;
   }
 });
--- zh-cn/main.js
+++ zh-cn/main.js
@@ -9,9 +9,52 @@ var goal_hour = parseInt(href.substr(href.indexOf('hour=') + 5, href.indexOf('&m
 var goal_minute = parseInt(href.substr(href.indexOf('minute=') + 7, href.indexOf('&second=') - href.indexOf('minute=') - 7));
 var goal_second = parseInt(href.substr(href.indexOf('second=') + 7, href.indexOf('&repeat_time=') - href.indexOf('minute=') - 7));
 var repeat_time = parseInt(href.substr(href.indexOf('repeat_time=') + 12, href.indexOf('&repeat_unit=') - href.indexOf('repeat_time=') - 12));
-var repeat_unit = href.substr(href.indexOf('repeat_unit=') + 12);
+var repeat_unit = href.substr(href.indexOf('repeat_unit=') + 12, href.indexOf('&if_add=') - href.indexOf('repeat_unit=') - 12);
+var date_type = href.substr(href.indexOf('date_type=') + 10, href.indexOf('&if_add=') - href.indexOf('date_type=') - 10);
+var if_add = parseInt(href.substr(href.indexOf('if_add=') + 7, href.indexOf('&if_end=') - href.indexOf('if_add=') - 7));
+var if_end = parseInt(href.substr(href.indexOf('if_end=') + 7, href.indexOf('&end_year=') - href.indexOf('if_end=') - 7));
+var end_year = parseInt(href.substr(href.indexOf('end_year=') + 9, href.indexOf('&end_month=') - href.indexOf('end_year=') - 9));
+var end_month = parseInt(href.substr(href.indexOf('end_month=') + 10, href.indexOf('&end_date=') - href.indexOf('end_month=') - 10));
+var end_date = parseInt(href.substr(href.indexOf('end_date=') + 9));
+var end_temp;
+// 农历转换
+function getLeapMonth(lunarYear) {
+  var index = lunarYear - 1900;
+  return lunarInfo[index] & 0xf;
+}
+if (date_type == 'lunar') {
+  var leap_month = getLeapMonth(goal_year);
+  var ifLeap = false;
+  if (leap_month > 0 && goal_month > leap_month) goal_month -= 1;
+  if (goal_month === leap_month) ifLeap = true;
+  goal.setFullYear(goal_year, goal_month - 1, goal_date);  
+  var cc = new CalendarConverter();
+  var solar = cc.lunar2solar(goal, ifLeap);
+  var lunar_year = solar.cYear + solar.lunarYear + '年(' + solar.lYear + ')';
+  var lunar_month = solar.lunarMonth + '月';
+  var lunar_date = solar.lunarDay;
+  goal_year = solar.sYear;
+  goal_month = solar.sMonth;
+  goal_date = solar.sDay + 1;
+  var leap_month = getLeapMonth(end_year);
+  var ifLeap = false;
+  if (leap_month > 0 && end_month > end_month) end_month -= 1;
+  if (end_month === leap_month) ifLeap = true;
+  goal.setFullYear(end_year, end_month - 1, end_date);  
+  var cc = new CalendarConverter();
+  var solar = cc.lunar2solar(goal, ifLeap);
+  var lunar_end_year = solar.cYear + solar.lunarYear + '年(' + solar.lYear + ')';
+  var lunar_end_month = solar.lunarMonth + '月';
+  var lunar_end_date = solar.lunarDay;
+  end_year = solar.sYear;
+  end_month = solar.sMonth;
+  end_date = solar.sDay + 1;
+}
+// 其它初始设置
 goal.setFullYear(goal_year, goal_month - 1, goal_date);
 goal.setHours(goal_hour, goal_minute, goal_second);
+var end = new Date();
+end.setFullYear(end_year, end_month - 1, end_date);
 var count = 0;
 
 // 自动设置字体大小
@@ -44,25 +87,44 @@ function refresh() {
     goal.setFullYear(goal_year, goal_month - 1, goal_date);
   }
   var value = parseInt((goal - now) / 86400000);
+  var end_value = parseInt((now - end) / 86400000);
   const days = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
-  if (value > 0 || value == 0 && goal_date != now.getDate()) {
+  if (if_end === 1 && end_value >= 0) {
+    document.getElementById("title_text").innerHTML = title + ' 共';
+    document.getElementById("title").style.backgroundColor = "orange";
+    document.getElementById("number").innerHTML = parseInt((end - goal) / 86400000);
+    if (date_type == 'lunar') document.getElementById("explain_text").innerHTML = lunar_year + ' ' + lunar_month + ' ' + lunar_date + '  ' + days[goal.getDay()] + ' ~ ' + lunar_end_year + ' ' + lunar_end_month + ' ' + lunar_end_date + '  ' + days[end.getDay()];
+    else document.getElementById("explain_text").innerHTML = goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()] + ' ~ ' + end_year + ' 年 ' + end_month + ' 月 ' + end_date + ' 日  ' + days[end.getDay()];
+    end_temp = goal;
+    goal = end;
+  } else if (value <= 0 && if_add === 1) {
+    document.getElementById("title_text").innerHTML = title + ' 第';
+    document.getElementById("title").style.backgroundColor = "orange";
+    document.getElementById("number").innerHTML = -value + 1;
+    if (date_type == 'lunar') document.getElementById("explain_text").innerHTML = '起始日：' + lunar_year + ' ' + lunar_month + ' ' + lunar_date + '  ' + days[goal.getDay()];
+    else document.getElementById("explain_text").innerHTML = '起始日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
+  } else if (value > 0 || value == 0 && goal_date != now.getDate()) {
     document.getElementById("title_text").innerHTML = title + ' 还有';
     document.getElementById("title").style.backgroundColor = "blue";
-    document.getElementById("number").innerHTML = value;
-    document.getElementById("explain_text").innerHTML = '目标日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
+    document.getElementById("number").innerHTML = (value == 0) ? 1 : value;
+    if (date_type == 'lunar') document.getElementById("explain_text").innerHTML = '目标日：' + lunar_year + ' ' + lunar_month + ' ' + lunar_date + '  ' + days[goal.getDay()];
+    else document.getElementById("explain_text").innerHTML = '目标日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
   } else if (value < 0 || value == 0 && goal_date != now.getDate()) {
     document.getElementById("title_text").innerHTML = title + ' 已经';
     document.getElementById("title").style.backgroundColor = "orange";
-    document.getElementById("number").innerHTML = -value;
-    document.getElementById("explain_text").innerHTML = '起始日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
+    document.getElementById("number").innerHTML = (value == 0) ? 1 : -value;
+    if (date_type == 'lunar') document.getElementById("explain_text").innerHTML = '起始日：' + lunar_year + ' ' + lunar_month + ' ' + lunar_date + '  ' + days[goal.getDay()];
+    else document.getElementById("explain_text").innerHTML = '起始日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
   } else {
     document.getElementById("title_text").innerHTML = title + ' 就是今天';
     document.getElementById("title").style.backgroundColor = "red";
     document.getElementById("number").innerHTML = value;
-    document.getElementById("explain_text").innerHTML = '目标日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
+    if (date_type == 'lunar') document.getElementById("explain_text").innerHTML = '目标日：' + lunar_year + ' ' + lunar_month + ' ' + lunar_date + '  ' + days[goal.getDay()];
+    else document.getElementById("explain_text").innerHTML = '目标日：' + goal_year + ' 年 ' + goal_month + ' 月 ' + goal_date + ' 日  ' + days[goal.getDay()];
   }
   document.getElementById("unit").innerHTML = '天';
   document.getElementById("explain_text_time").innerHTML = String(goal_hour).padStart(2,'0') + ':' + String(goal_minute).padStart(2,'0') + ':' + String(goal_second).padStart(2,'0');
+  if (if_end === 1 && end_value >= 0) document.getElementById("explain_text_time").innerHTML = '';
   count = 0;
   font_size();
 }
@@ -96,38 +158,54 @@ function refresh_second() {
 }
 // 更换格式
 function format() {
-  count = (count + 1) % 5;
+  count = (if_end === 1) ? (count + 1) % 4 : (count + 1) % 5;
   if (count == 0) {
     setTimeout(() => {
-      var now = new Date();
+      var now = (if_end === 1) ? end_temp : new Date();
       var value = parseInt((goal - now) / 86400000);
-      document.getElementById("number").innerHTML = (value >= 0) ? value : -value;
+      value = (value >= 0) ? value : -value;
+      document.getElementById("number").innerHTML = (value == 0) ? 1 : value;
       document.getElementById("unit").innerHTML = '天';
       font_size();
-    }, 500);    
+    }, 500);
   } else if (count == 1) {
-    var now = new Date();
+    var now = (if_end === 1) ? end_temp : new Date();
     var value1 = parseInt((goal - now) / 31536000000);
     var value2 = parseInt(((goal - now) / 86400000 - value1 * 365) / 30);
     var value3 = parseInt((goal - now) / 86400000 - value1 * 365 - value2 * 30);
     value1 = (value1 >= 0) ? value1 : -value1;
     value2 = (value2 >= 0) ? value2 : -value2;
     value3 = (value3 >= 0) ? value3 : -value3;
+    if (value1 === 0) {
+      format();
+      return;
+    }
     document.getElementById("number").innerHTML = value1 + '年' + value2 + '个月' + value3 + '天';
     document.getElementById("unit").innerHTML = '';
   } else if (count == 2) {
-    var now = new Date();
+    var now = (if_end === 1) ? end_temp : new Date();
     var value1 = parseInt((goal - now) / 2592000000);
     var value2 = parseInt((goal - now) / 86400000 - value1 * 30);
     value1 = (value1 >= 0) ? value1 : -value1;
     value2 = (value2 >= 0) ? value2 : -value2;
+    if (value1 === 0) {
+      format();
+      return;
+    }
     document.getElementById("number").innerHTML = value1 + '个月' + value2 + '天';
     document.getElementById("unit").innerHTML = '';
   } else if (count == 3) {
-    var now = new Date();
-    var value = parseInt((goal - now) / 604800000);
-    document.getElementById("number").innerHTML = (value >= 0) ? value : -value;
-    document.getElementById("unit").innerHTML = '周';
+    var now = (if_end === 1) ? end_temp : new Date();
+    var value1 = parseInt((goal - now) / 604800000);
+    var value2 = parseInt((goal - now) / 86400000 - value1 * 7);
+    value1 = (value1 >= 0) ? value1 : -value1;
+    value2 = (value2 >= 0) ? value2 : -value2;
+    if (value1 === 0) {
+      format();
+      return;
+    }
+    document.getElementById("number").innerHTML = value1 + '周' + (value2 > 0 ? (value2 + '天') : '');
+    document.getElementById("unit").innerHTML = '';
   } else if (count == 4) {
     var now = new Date();
     var value1 = parseInt((goal - now) / 86400000);
@@ -150,7 +228,23 @@ function format() {
 
 // 保存到文件
 function save() {
-  var text = title + '\n' + goal_year + '\n' + goal_month + '\n' + goal_date + '\n' + goal_hour + '\n' + goal_minute + '\n' + goal_second + '\n' + repeat_time + '\n' + repeat_unit;
+  var text;
+  if (date_type == 'lunar') {
+    var cc = new CalendarConverter();
+    if (if_end === 1) {
+      end = goal;
+      goal = end_temp;
+    }
+    var lunar = cc.solar2lunar(goal);
+    goal_year = lunar.lYear;
+    goal_month = lunar.lMonth;
+    goal_date = lunar.lDay;
+    var lunar = cc.solar2lunar(end);
+    end_year = lunar.lYear;
+    end_month = lunar.lMonth;
+    end_date = lunar.lDay;
+  }
+  text = title + '\n' + goal_year + '\n' + goal_month + '\n' + goal_date + '\n' + goal_hour + '\n' + goal_minute + '\n' + goal_second + '\n' + repeat_time + '\n' + repeat_unit + '\n' + date_type + '\n' + if_add + '\n' + if_end + '\n' + end_year + '\n' + end_month + '\n' + end_date;
   var blob = new Blob([text], {type: 'text/plain'});
   var downloadLink = document.createElement('a');
   downloadLink.setAttribute('href', window.URL.createObjectURL(blob));
